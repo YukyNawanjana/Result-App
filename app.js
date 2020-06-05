4ef063411d47641acdf6a571e166fc40a3e8f8a3
YukyNawanjana <yasirunawanjana70@gmail.com>
2020-06-05 08:51:37 +0530

    Update

--- app.js
+++ app.js
@@ -8,32 +8,41 @@ btn.addEventListener('click', function(e){
     const marksvlaue = marks.value;
     
     if(marksvlaue){
-        displayResult(marksvlaue);
+        if(marksvlaue >= 90){
+            alert.textContent = "Your Result is A+";
+            alert.style.display ="block";
+            alert.classList.add('alerts-primary');
+
+        }else if(marksvlaue >=75){
+            alert.textContent = "Your Result is A";
+            alert.style.display ="block";
+            alert.classList.add('alerts-info');
+
+        }else if(marksvlaue >=65){
+            alert.textContent = "Your Result is B";
+            alert.style.display ="block";
+            alert.classList.add('alerts-success');
+
+        }else if(marksvlaue >=55){
+            alert.textContent = "Your Result is C";
+            alert.style.display ="block";
+            alert.classList.add('alerts-secondary');
+
+        }else if(marksvlaue >=35){
+            alert.textContent = "Your Result is S";
+            alert.style.display ="block";
+            alert.classList.add('alerts-warning');
+           
+        }else{
+            alert.textContent = "Sorry, You are Exam Fail";
+            alert.style.display ="block";
+            alert.classList.add('alerts-danger');
+            
+        }
     }else{
-       displayAlert('Sorry Please Enter Your Marks',"danger");
+        alert.textContent = "Sorry Please Enter Your Marks";
+        alert.style.display ="block";
+        alert.classList.add('alerts-danger');
     }
     marks.value="";
 });
-
-
-function displayResult(marksvlaue){
-    if(marksvlaue >= 90){
-        displayAlert("Your Result is A+", "primary");
-    }else if(marksvlaue >=75){
-        displayAlert("Your Result is A", "info");
-    }else if(marksvlaue >=65){
-        displayAlert("Your Result is B", "success");
-    }else if(marksvlaue >=55){
-        displayAlert("Your Result is C", "secondary");
-    }else if(marksvlaue >=35){
-        displayAlert("Your Result is S", "warning");
-    }else{
-        displayAlert("Sorry, You are Exam Fail", "danger");
-    }
-}
-
-function displayAlert(text, action){
-    alert.textContent = text;
-    alert.style.display ="block";
-    alert.classList.add(`alert-${action}`);
-}
